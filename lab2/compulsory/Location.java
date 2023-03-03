7b8724bdc6568e99d02ca43971b4f802606def57
gabrielachirila <99080710+gabrielachirila@users.noreply.github.com>
2023-03-03 19:32:37 +0200

    Create Location.java

---
+++ lab2/compulsory/Location.java
@@ -0,0 +1,62 @@
+
+public class Location {
+    private String name;
+    private double x;
+    private double y;
+    private LocationType type;
+
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getX() {
+        return x;
+    }
+
+    public void setX(double x) {
+        this.x = x;
+    }
+
+    public double getY() {
+        return y;
+    }
+
+    public void setY(double y) {
+        this.y = y;
+    }
+
+    public LocationType getType() {
+        return type;
+    }
+
+    public void setType(LocationType type) {
+        this.type = type;
+    }
+
+    public Location() {}
+
+    public Location(String name) {
+        this.name = name;
+    }
+    public Location(String name, double x, double y, LocationType type) {
+        this.name = name;
+        this.x = x;
+        this.y = y;
+        this.type = type;
+    }
+
+    @Override
+    public String toString() {
+        return "Location{" +
+                "name='" + name + '\'' +
+                ", x=" + x +
+                ", y=" + y +
+                ", type=" + type +
+                '}';
+    }
+}
