6725e47fbffac7e7d864a52f3b8554a5b60222fc
gabrielachirila <99080710+gabrielachirila@users.noreply.github.com>
2023-03-17 23:03:19 +0200

    Add files via upload

---
+++ laboratory4/compulsory/Student.java
@@ -0,0 +1,33 @@
+package org.example;
+
+public class Student implements Comparable<Student> {
+    private String name;
+
+    public Student() {
+    }
+
+    public Student(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public int compareTo(Student o) {
+        return name.compareTo(o.getName());
+    }
+
+    @Override
+    public String toString() {
+        final StringBuffer sb = new StringBuffer("Student{");
+        sb.append("name='").append(name).append('\'');
+        sb.append('}');
+        return sb.toString();
+    }
+}
