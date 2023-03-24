2e930db54041e5e27a44e509f256517f99328a0b
Chirila Gabriela <99080710+gabrielachirila@users.noreply.github.com>
2023-03-24 19:32:21 +0200

    Update Main.java

--- laboratory4/homework/src/main/java/org/example/Main.java
+++ laboratory4/homework/src/main/java/org/example/Main.java
@@ -8,55 +8,6 @@ import java.util.stream.*;
 public class Main {
     public static void main(String[] args) {
 
-//        var projects = IntStream.rangeClosed(0, 2)
-//                .mapToObj(i -> new Project("P" + i) )
-//                .toArray(Project[]::new);
-//
-//        SortedSet<Project> listOfProjects = new TreeSet<>();
-//
-//        listOfProjects.addAll( Arrays.asList(projects) );
-//
-//        List<Student> listOfStudents = new LinkedList<>();
-//        //List<Project> listOfPreferences = new LinkedList<>();
-//
-//        Student s1 = new Student("Erika");
-//        s1.addStudentPreference(projects[0]);
-//        s1.addStudentPreference(projects[1]);
-//        s1.addStudentPreference(projects[2]);
-//
-//
-//        Student s2 = new Student("Cecilia");
-//        s2.addStudentPreference(projects[0]);
-//        s2.addStudentPreference(projects[1]);
-//
-//        Student s3 = new Student("Iosif");
-//        s3.addStudentPreference(projects[0]);
-//
-//        listOfStudents.add(s1);
-//        listOfStudents.add(s2);
-//        listOfStudents.add(s3);
-//
-//        System.out.println("Proiecte: ");
-//        System.out.println(listOfProjects);
-//        System.out.println("Studenti: ");
-//        System.out.println(listOfStudents);
-//
-//        Problem p1 = new Problem(listOfStudents,listOfProjects);
-
-//        double averageNumberOfPreferences = p1.listOfStudents.stream()
-//                .mapToInt(s -> s.getStudentPreferences().size())
-//                .average()
-//                .orElse(0);
-//
-//        System.out.println("Average number of preferences: " +  averageNumberOfPreferences);
-//
-//        List<Student> result = p1.listOfStudents.stream()
-//                .filter(s -> s.getStudentPreferences().size() < averageNumberOfPreferences)
-//                .collect(Collectors.toList());
-//
-//        System.out.println("All the students that have a number of preferences lower than the average number of preferences: ");
-//        result.forEach(System.out::println);
-
         Faker faker = new Faker();
 
         Project project1 = new Project(faker.name().username());
