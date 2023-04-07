15f31eff940e25432f2c3d5094a29b979681fc89
Chirila Gabriela <99080710+gabrielachirila@users.noreply.github.com>
2023-04-07 11:52:38 +0300

    Create Main.java

---
+++ laboratory6/compulsory/Main.java
@@ -0,0 +1,10 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+
+        MainFrame game = new MainFrame();
+        game.setSize(800,700);
+        game.setVisible(true);
+    }
+}
